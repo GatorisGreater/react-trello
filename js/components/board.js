a31d3d70d1a25ab5155b239e895dc0ab47a73a76
gatorisgreater <william.broadway@gmail.com>
2017-01-10 19:37:36 -0500

    Updated with progress towards implenting appropriate parent-child framework

--- js/components/board.js
+++ js/components/board.js
@@ -3,17 +3,50 @@ import ReactDOM from 'react-dom';
 
 import List from './list';
 
-export default function Board() {
-	var props = { title:'Things to do', lists:['breakfast','lunch','Dinner']};
-	console.log(props);
-	return (
-		<div className="list-board">
-			<div className="board-title">
-			 {props.title} 
-				<List > {props.lists[0]} </List>
-				<List > {props.lists[1]} </List>
-				<List > {props.lists[2]} </List>
+// const cards = ["I'll eat that again", "I wouldn't eat that again", "My dog wouldn't eat that"];
+
+export default function Board(props) {
+	// const lists = props.lists.map(list =>
+	// 	<List title={props.lists[0]}
+	// 		  cards={cards} />
+
+	// );
+	let cardTitles = props.cards;
+	console.log(props.cards);
+
+return(
+		<div className="board-title">{props.title} 
+			<div className="board-list">{props.lists[0]}
+				<div className="board-card" cards={props.cards}></div>
 			</div>
-		</div>
-		)
-};
+			<div className="board-list">{props.lists[1]}
+				<div className="board-card" cards={props.cards}></div>
+			</div>
+			<div className="board-list">{props.lists[2]}
+				<div className="board-card" cards={props.cards}></div>
+			</div>
+		</div>	
+)};
+
+
+
+//
+// export default function Board(props) {
+// 	var props = { title:'Things to do', lists:['breakfast','lunch','Dinner']};
+// 	console.log(props);
+	
+
+
+// 	return (
+// 		<div className="list-board">
+// 			<div className="board-title">
+// 			 {props.title} 
+
+// 			 //Pass title and card property to each List element using the map method.
+// 				<List title="blah"> {props.list[0]} </List>
+// 				<List > {props.lists[1]} </List>
+// 				<List > {props.lists[2]} </List>
+// 			</div>
+// 		</div>
+// 		)
+// };
--- js/components/list.js
+++ js/components/list.js
@@ -1,28 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import Card from './cards';
 
 
-export default  function List  () {
-	var props = {title:"Title of the card", cards:"Content of cards here", lists:['breakfast','lunch','dinner']};
-	console.log(props);
+
+export default function List (props) {
+	console.log(props.cards);
 	return(
-		<div className="card-list">
 		<div className="list-title">
-		{props.title}
-		<div className="list-lists">
-		{props.lists[0]}
-		<Card>{props.cards}</Card>
+			<div className="list-card">{props.cards[0]}</div>
+			<div className="list-card">{props.cards[1]}</div>
+			<div className="list-card">{props.cards[2]}
+			</div>
 		</div>
-		<div className="list-lists">
-		{props.lists[1]}
-		<Card>{props.cards}</Card>
-		</div>
-		<div className="list-lists">
-		{props.lists[2]}
-		<Card>{props.cards}</Card>
-		</div>
-		</div>
-		</div>
-		)
-};
+	)
+}
+
+
+
+
+// export default  function List  (props) {
+// 	var props = {title:"Title of the card", cards:"Content of cards here", lists:['breakfast','lunch','dinner']};
+// 	console.log(props);
+// 	return(
+// 		<div className="card-list">
+// 		<div className="list-title">
+// 		{props.title}
+// 		<div className="list-lists">
+// 		{props.lists[0]}
+// 		<Card>{props.cards}</Card>
+// 		</div>
+// 		<div className="list-lists">
+// 		{props.lists[1]}
+// 		<Card>{props.cards}</Card>
+// 		</div>
+// 		<div className="list-lists">
+// 		{props.lists[2]}
+// 		<Card>{props.cards}</Card>
+// 		</div>
+// 		</div>
+// 		</div>
+// 		)
+// };

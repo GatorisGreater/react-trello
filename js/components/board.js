d4fc5ca78f85785c9eda9b7ea7d23542335f635f
gatorisgreater <william.broadway@gmail.com>
2017-01-11 06:42:12 -0500

    added formatting

--- js/components/board.js
+++ js/components/board.js
@@ -7,11 +7,11 @@ export default function Board(props) {
 
 return(
 		<div className="board-title">{props.title} 
-			<div className="lists-list"> {props.lists[0]}
+			<div className="lists-list"> <p> {props.lists[0]} </p>
 				<List cards={props.cards} text={props.text}></List></div>
-			<div className="lists-list"> {props.lists[1]}
+			<div className="lists-list"> <p> {props.lists[1]} </p>
 				<List cards={props.cards} text={props.text}></List></div>
-			<div className="lists-list"> {props.lists[2]}
+			<div className="lists-list"> <p> {props.lists[2]} </p>
 				<List cards={props.cards} text={props.text}></List>
 			</div>
 		</div>	
--- js/components/list.js
+++ js/components/list.js
@@ -6,11 +6,11 @@ import Card from './cards';
 export default function List (props) {
 
 	return(
-		<div className="card-list">
-			<div className="card-text">{props.cards[0]}<Card text={props.text}></Card></div>
-			<div className="card-text">{props.cards[1]}<Card text={props.text}></Card></div>
-			<div className="card-text">{props.cards[2]}<Card text={props.text}></Card>
+		<div className="card-list"> <ul>
+			<div className="card-text"><li>{props.cards[0]}</li><Card text={props.text}></Card></div>
+			<div className="card-text"><li>{props.cards[1]}</li><Card text={props.text}></Card></div>
+			<div className="card-text"><li>{props.cards[2]}</li><Card text={props.text}></Card>
 			</div>
-		</div>
+		</ul></div>
 	)
 }
